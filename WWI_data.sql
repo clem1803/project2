978119a3f547e7f3ee9d61a598a6cfe2ed608427
clem1803 <clementboit@gmail.com>
2021-06-08 21:56:17 -0400

    data clean up and short SQL work

---
+++ WWI_data.sql
@@ -0,0 +1,12 @@
+create table WWI_aircraft_data(
+Aircraft_id int not null,
+AIRCRAFT varchar (1200) not null,
+FULL_NAME varchar (1200) not null,
+Aircraft_type varchar (1200) not null,
+HYPERLINK varchar (1200) not null
+);
+
+select *
+from WWI_aircraft_data;
+
+DROP TABLE WWI_aircraft_data;
